32a27b546186325a589576ed0d9e5ae20f1d5711
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-17 20:25:02 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -14,7 +14,7 @@ const GetRemoteDataHandler = {
 
     await getRemoteData('http://177.55.114.52/dash/teste_conexao_sql.php?local=963')
       .then((response) => {
-        const data = JSON.parse(response);
+        const data = 0;
         outputSpeech = `Estão sendo executadas ${data.number} no local. `;
  //       for (let i = 0; i < data.people.length; i += 1) {
      //     if (i === 0) {
